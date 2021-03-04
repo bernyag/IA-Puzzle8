51995b070959acf99a094310a781f1ade3f0ad55
Bernardo <berny.ag30@gmail.com>
2021-03-03 19:21:57 -0600

    changes on manhattan, it now gets costs of all movements

--- Puzzle8.lisp
+++ Puzzle8.lisp
@@ -1,45 +1,50 @@
-(setq goal '( 1 2 3 4 5 6 7 8 0))
+(setq goal '(0 1 2 3 4 5 6 7 8))
 (setq current '( 1 3 5 7 8 6 4 2 0))
 
 (defun up (current zeroPos)
   (let ((aux (copy-list current)))
-    (if (and (> zeroPos 2) (<= zeroPos 8))
-        (rotatef (elt aux zeroPos) (elt aux (- zeroPos 3)))
-    )
+    ;(if (and (> zeroPos 2) (<= zeroPos 8))
+    (rotatef (elt aux zeroPos) (elt aux (- zeroPos 3)))
     aux
   )
 )
 
 (defun down (current zeroPos)
   (let ((aux (copy-list current)))
-    (if (and (<= zeroPos 5))
-        (rotatef (elt aux zeroPos) (elt aux (+ zeroPos 3)))
-    )
+    ;(if (and (<= zeroPos 5))
+    (rotatef (elt aux zeroPos) (elt aux (+ zeroPos 3)))
     aux
   )
 )
 
 (defun right (current zeroPos)
   (let ((aux (copy-list current)))
-    (if (and (not (or (= zeroPos 2) (= zeroPos 5) (= zeroPos 8))) (<= zeroPos 8))
-        (rotatef (elt aux zeroPos) (elt aux (+ zeroPos 1)))
-    )
+    ;(if (and (not (or (= zeroPos 2) (= zeroPos 5) (= zeroPos 8))) (<= zeroPos 8))
+    (rotatef (elt aux zeroPos) (elt aux (+ zeroPos 1)))
     aux
   )
 )
 
 (defun left (current zeroPos)
   (let ((aux (copy-list current)))
-    (rotatef (elt current zeroPos) (elt current (- zeroPos 1)))
-    current
+    ;(if (and (not (or (= (position 0 current) 0) (= (position 0 current) 3) (= (position 0 current) 6))) (<= (position 0 current) 8))
+    (rotatef (elt aux zeroPos) (elt aux (- zeroPos 1)))
+    aux
   )
 )
 
-(setq varTotalCost 0)
 (defun getTotalCost (current)
+    (if (not (null current) ) 
+        (+ (getTotalCost (cdr current ) ) (getCost  (- 9 (length current) ) (car current) ) )  
+        0
+    )
+)
+;versiónn usando var globa. DEPRECATED
+(setq varTotalCost 0)
+(defun getTotalCost2 (current)
     (cond 
         ((null current))
-        (t (incf varTotalCost (getCost  (- 9 (length current)) (car current) ) ) (getTotalCost (cdr current)))
+        (t (incf varTotalCost (getCost  (- 9 (length current)) (car current) ) ) (getTotalCost2 (cdr current)))
     )
   varTotalCost
 )
@@ -74,11 +79,25 @@
 (setq downCost 0)
 (setq rightMan 0)
 (defun manhattan (current nodeNumber father level cost direction)
+
   ;checa si puede mover a la izquierda, en dado caso lo hace
   (if (and (not (or (= (position 0 current) 0) (= (position 0 current) 3) (= (position 0 current) 6))) (<= (position 0 current) 8))
-    (setq varTotalCost 0) 
-    (setq leftCost (getTotalCost (left current (position 0 current))) ) ; TODO: arreglar a que regrese un número
+    (setq leftCost (getTotalCost (left current (position 0 current))))
+  )
     ;(manhattan (left current (position 0 current)) (+ nodeNumber 1) nodeNumber (+ level 1) (+ cost (getTotalCost current)) 1) 
+  
+  ;checa si puede mover a la derecha, en dado caso lo hace
+  (if (and (not (or (= (position 0 current) 2) (= (position 0 current) 5) (= (position 0 current) 8))) (<= (position 0 current) 8))
+    (setq rightCost (getTotalCost (right current (position 0 current))))
+  )
+
+  ;checa si puede mover abajo, en dado caso lo hace
+  (if (and (<= (position 0 current) 5))
+    (setq downCost (getTotalCost (down current (position 0 current))))
+  )
+
+  ;checa si puede mover arriba, en dado caso lo hace
+  (if (and (> (position 0 current) 2) (<= (position 0 current) 8))
+    (setq upCost (getTotalCost (up current (position 0 current))))
   )
 )
-
